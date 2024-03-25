bb4493ccd5b147aaec838048738adaf2f9e4e8fd
DESBOIS-RENAUDIN Meo <Meo.Desbois-Renaudin@bordeaux-inp.fr>
2024-03-25 15:38:14 +0100

    inverse implementation

--- control.py
+++ control.py
@@ -1,27 +1,6 @@
 import math
 import numpy as np
-import meshcat.transformations as tf
-l1, l2, l3 = 0.045, 0.065, 0.087
-l = 0.04
-def floorBtw(min, max, value):
-    if value < min:
-        return min
-    if value >=max:
-        return max
-    return value
 
-def radToDeg(rad):
-    return rad * 180 / math.pi
-
-def degToRad(deg):
-    return deg * math.pi / 180
-
-def alKashi(a, b, c):
-    """
-    Renvoie l'angle en radians du triangle rectangle dont les côtés opposés aux angles sont de longueur a, b et c
-    """
-    result = floorBtw(-1,1,((a*a + b*b - c*c) / (2*a*b)))
-    return math.acos(result)
 def sandbox(t):
     """
     python simulator.py -m sandbox
@@ -37,7 +16,9 @@ def sandbox(t):
 
     # Par exemple, on envoie un mouvement sinusoidal
     targets = [0]*12
-    targets[0] = np.sin(t)
+    targets[0] = 0
+    targets[1] = np.pi/6
+    targets[2] = -np.pi/2 + np.pi/6
 
     return targets
 
@@ -55,14 +36,7 @@ def direct(alpha, beta, gamma):
     - Sortie: un tableau contenant la position atteinte par le bout de la patte (en mètres)
     """
 
-    xp = l1 + math.cos(beta)*l2 + math.cos(beta + gamma)*l3
-    yp = math.sin(beta)*l2 + math.sin(beta + gamma)*l3
-    
-    x = math.cos(alpha) * xp 
-    y = math.sin(alpha) * xp 
-    z = yp
-    
-    return x, y, z
+    return [0., 0., 0.]
 
 def inverse(x, y, z):
     """
@@ -77,43 +51,40 @@ def inverse(x, y, z):
     - Entrée: x, y, z, une position cible dans le repère de la patte (mètres), provenant du slider
     - Sortie: un tableau contenant les 3 positions angulaires cibles (en radians)
     """
-    d = math.sqrt(x**2 + y**2)
-    ac = math.sqrt((d - l1)**2 + z**2)
+    l1 = 0.049
+    l2 = 0.065
+    l3 = 0.095
+    
+    alpha = math.atan2(y, x)
+    
+    x_a = l1*math.cos(alpha)
+    y_a = l1*math.sin(alpha)
+    
+    D = math.sqrt((x - x_a)**2 + (y - y_a)**2 + z**2)
 
-    theta1 = -math.atan2(y, x)
+    beta = 0
+    if (l2 != 0 and D!=0):
+        beta = math.acos( np.clip((l2**2 + D**2 - l3**2)/(2*D*l2), -1, 1)) + math.asin(z/D)
 
-    gamma = math.atan2(z, d - l1) 
-    beta = alKashi(ac, l2, l3)
-    theta2 = beta + gamma
+    if (l2!=0 and l3!=0):
+        gamma = math.acos( np.clip( (D**2-l2**2 -l3**2)/(2*l2*l3), -1, 1))
 
-    theta3 = math.pi - alKashi(l2, l3, ac) - math.pi/2
+    # hexapod version
+    alpha = -alpha
 
-    return [theta1, theta2, theta3]
+    tmp_angle = np.pi/6
 
-def interpolation_lineaire_3d(x,y, t):
-    """
-    Interpolation linéaire en trois dimensions entre deux points (x0, y0, z0) et (x1, y1, z1) selon un paramètre t.
+    PHASE = np.array([0, tmp_angle, -np.pi/2+tmp_angle])
 
-    Arguments :
-    - x0, y0, z0 : coordonnées du premier point
-    - x1, y1, z1 : coordonnées du deuxième point
-    - t : paramètre d'interpolation (compris entre 0 et 1)
+    res = np.array([alpha, beta, gamma]) 
+    res += PHASE
 
-    Sortie :
-    - Les coordonnées interpolées (x, y, z) entre les deux points en fonction de t
-    """
-    x0,y0,z0 = x
-    x1,y1,z1 = y
+    return list(res)
 
-    xr = x0 + (x1 - x0) * t
-    yr = y0 + (y1 - y0) * t
-    zr = z0 + (z1 - z0) * t
+def interpolate(p1, p2, t):
+    return p1 + (p2 - p1) * t
+    
 
-
-    return xr, yr, zr
-
-
-from interpolation import Interpolate
 def draw(t):
     """
     python simulator.py -m draw
@@ -127,48 +98,18 @@ def draw(t):
     - Sortie: un tableau contenant les 3 positions angulaires cibles (en radians)
     """
 
-    duration = 10
-
-    point1 = (0.1, -0.07, -0.07)
-    point2 = (0.1, 0, 0.07)
-    point3 = (0.1, 0.07,-0.07)
+    key_points = [(0.1, -0.1, 0), (0.1, 0., 0.1), (0.1, 0.1, 0.)]
     
-    t = t % duration
-    t = t / duration
+    # Perform linear interpolation between the key points
+    # Find the appropriate segment based on time
+    segment_idx = int(t) % len(key_points)
+    t_in_segment = t % 1
 
-    interpolate = Interpolate([point1,point2,point3,point1])
-    interpolated_point = interpolate.lerp(t)
+    # Calculate the target positions
+    target_positions = interpolate(np.array(key_points[segment_idx]), np.array(key_points[(segment_idx+1) % len(key_points)]), t_in_segment)
 
-    return inverse(interpolated_point[0], interpolated_point[1], interpolated_point[2])
-from math import pi
-
-def getLegsCoord(x,y,z,i):
-    
-  
-    legsAlpha= [ degToRad(135),degToRad(-135), degToRad(-45), degToRad(45)]
-    legsOrigin =[
-        [math.cos(legsAlpha[0])*l, math.sin(legsAlpha[0])*l, 0],
-        [math.cos(legsAlpha[1])*l, math.sin(legsAlpha[1])*l, 0],
-        [math.cos(legsAlpha[2])*l, math.sin(legsAlpha[2])*l, 0],
-        [math.cos(legsAlpha[3])*l, math.sin(legsAlpha[3])*l, 0]
-
-    ]   
-    
-    translate = tf.translation_matrix(legsOrigin[i]) \
-        @ tf.rotation_matrix(legsAlpha[i], [0, 0, 1]) \
-    
-    inversed = tf.inverse_matrix(translate)
- 
- 
-    point = np.array([
-        [x],
-        [y],
-        [z],
-        [1.]])
-    output = np.dot(inversed, point)
-
-    return output[0][0], output[1][0], output[2][0]
-
+    (x, y, z) = target_positions
+    return inverse(x, y, z)
 
 def legs(targets_robot):
     """
@@ -180,27 +121,49 @@ def legs(targets_robot):
     - Entrée: des positions cibles (tuples (x, y, z)) pour le bout des 4 pattes
     - Sortie: un tableau contenant les 12 positions angulaires cibles (radian) pour les moteurs
     """
+
+    # positions in legs bases
+    leg_based_coordinates = [(0.0, 0., 0.)]*4
+
+    phi = 5*np.pi/4
+    for i in range(4):
+        A = np.array(  [ [np.cos(phi), -np.sin(phi), 0],
+                         [np.sin(phi),  np.cos(phi), 0],
+                         [          0,            0, 1]])
+
+        leg_based_coordinates[i] = tuple(np.dot(A, targets_robot[i]) + (-0.02, 0., 0.))
+        
+        phi -= np.pi/2
+
+    print();
+
     targets = [0]*12
 
     for i in range(4):
-        #convert to leg coordinates
-        x, y, z = getLegsCoord(targets_robot[i][0], targets_robot[i][1], targets_robot[i][2], i)
-        #inverse kinematics
-        angles = inverse(x, y, z)
-        #add to targets
-        targets[i*3] = angles[0]
-        targets[i*3 + 1] = angles[1]
-        targets[i*3 + 2] = angles[2]
-
+        (x, y, z) = leg_based_coordinates[i]
+        tmp = inverse(x, y, z)
+        for j in range(3):
+            targets[3*i+j] = tmp[j]
 
     return targets
 
 
 
-initial_gap= 0.10
+def triangle(target_angle):
+    triangle = [(-0.1, 0., 0), (0., 0., 0.1), (0.1, 0., 0.)]
+    phi = target_angle
+    A = np.array(  [ [np.cos(phi), -np.sin(phi), 0],
+                     [np.sin(phi),  np.cos(phi), 0],
+                     [          0,            0, 1]])
+    
+    newtriangle = [np.array([0.]*3)]*3
+    for i in range(len(triangle)):
+        newtriangle[i] = np.dot(A,triangle[i]) /2
+    
+    return newtriangle
+    
 
-def produit_vector(x,y):
-    return (x[0]*y[0], x[1]*y[1],  x[2]*y[2])
+        
 
 def walk(t, speed_x, speed_y, speed_rotation):
     """
@@ -213,87 +176,47 @@ def walk(t, speed_x, speed_y, speed_rotation):
             speed_x, speed_y, et speed_rotation, vitesses cibles contrôlées par les sliders
     - Sortie: un tableau contenant les 12 positions angulaires cibles (radian) pour les moteurs
     """
-    initial_height = 0.05
+    speed_multiplier = 300. * speed_x #15. * np.sqrt(speed_x*speed_x + speed_y*speed_y)
+    targets = [0]*12
+    legs_phase = [0., 1.5, 0., 1.5]
+    target_angle = 0 #20 * np.pi * speed_y #np.arctan(speed_y/(speed_x+0.000000001))
+    resting_pos = (0.1, 0., -0.1)
+    pattern_size = 20 * speed_y
 
-    init_1 = [-initial_gap, initial_gap, -initial_height]
-    init_2 = [-initial_gap, -initial_gap, -initial_height]
-    init_3 = [initial_gap, -initial_gap, -initial_height]
-    init_4 = [initial_gap, initial_gap, -initial_height]
+    tmp = triangle(target_angle)
 
+    movement_pattern = [(0., 0., 0.)]*3
+    for i in range(len(tmp)):
+        movement_pattern[i] = pattern_size * tmp[i]
 
+    # positions in legs bases
+    leg_based_coordinates = [(0., 0., 0.)]*4
+
+    phi = 5*np.pi/4
+    for i in range(4):
+        segment_idx = int(t*speed_multiplier+legs_phase[i]) % len(movement_pattern)
+        t_in_segment = (t*speed_multiplier+legs_phase[i]) % 1
+
+        target_position = interpolate(np.array(movement_pattern[segment_idx]), np.array(movement_pattern[(segment_idx+1) % len(movement_pattern)]), t_in_segment)
+
+        (x, y, z) = target_position
     
+        A = np.array(  [ [np.cos(phi), -np.sin(phi), 0],
+                         [np.sin(phi),  np.cos(phi), 0],
+                         [          0,            0, 1]])
 
-    duration = 1
-    shift = 0.8
+        leg_based_coordinates[i] = tuple(np.dot(A, target_position) + resting_pos)
+        
+        phi -= np.pi/2
 
-    
-    far_point = 0.1
-    near_point = far_point/4
-
-    
-
-    tri_point_1 = [
-        init_1,
-        (0, initial_gap, 0),
-        init_1,
-        produit_vector(init_1, (1, 1, 0)),
-        init_1,
-        init_1,
-        init_1
-    ]
-
-    tri_point_2 = [
-        init_2,
-        init_2,
-        init_2,
-        init_2,
-        (0, -initial_gap, 0),
-        init_2,
-        init_2
-    ]
-
-    tri_point_3 = [
-        init_3,
-        init_3,
-        init_3,
-        init_3,
-        init_3,
-        (initial_gap, -initial_gap/4, 0),
-        init_3
-    ]
-
-    tri_point_4 = [
-        init_4,
-        (far_point,near_point,0),
-        (initial_gap, 0, 0),
-        produit_vector(init_4, (1, 1, 0)),
-        init_4,
-        init_4,
-        init_4
-    ]
-
-    interp1 = Interpolate(tri_point_1)
-    interp3 = Interpolate(tri_point_3)
-    interp2 = Interpolate(tri_point_2)
-    interp4 = Interpolate(tri_point_4)
-    t_1 = (t%duration)/duration
-    t_2 = ((t+shift)%duration)/duration
-    point1 = interp1.lerp(t_1)
-    point3 = interp3.lerp(t_1)
-    point2 = interp2.lerp(t_1)
-    point4 = interp4.lerp(t_1)
+    for i in range(4):
+        (x, y, z) = leg_based_coordinates[i]
+        tmp = inverse(x, y, z)
+        for j in range(3):
+            targets[3*i+j] = tmp[j]
 
 
-
-
-    
-
-
-    return legs([point1, point2, point3, point4])
-    
-
-
-    
+    return targets
 
 if __name__ == "__main__":
     print("N'exécutez pas ce fichier, mais simulator.py")
